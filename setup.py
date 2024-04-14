5da1ae4464aa277d5a28a7859e1bc46bf98c4f73
Emmanuel Asiimwe <90935867+asiimwemmanuel@users.noreply.github.com>
2024-04-14 23:01:11 +0300

    updating setup.py to match PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from cx_Freeze import Executable
 # Project Metadata
 setup(
 	name='PolyTrend',
-	version='0.1.0',
+	version='1.0.0',
 	license='GPL-3.0',
 	description='Regression algorithm for polynomial function approximation',
 	long_description='PolyTrend is a regression algorithm that approximates and plots a polynomial function onto given data. It provides insights and conclusions in the fields of interpolation and polynomial regression, specifically in the subfield of approximation theory.',
@@ -17,20 +17,22 @@ setup(
 		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: GNU General Public License v3 (GPL-3)',
-		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.11',
 		'Operating System :: Microsoft :: Windows',
 		'Operating System :: POSIX :: Linux',
-		# 'Operating System :: Apple :: macOS'
+		'Operating System :: Apple :: macOS'
 	],
 	keywords='math, numerical-analysis, data-processing, statistics',
 	install_requires=[
-		'numpy',
-		'scikit-learn',
-		'matplotlib',
-		'pandas',
-		'PyQt6',
-		'PySide6'
+		'cx_Freeze==6.15.16',
+		'matplotlib==3.8.3',
+		'numpy==1.26.4',
+		'scipy==1.12.0',
+		'pandas==2.2.1',
+		'PyQt6==6.6.1',
+		'PyQt6_sip==13.6.0',
+		'scikit_learn==1.4.1.post1',
+		'setuptools==65.5.0'
 	],
 	options = {
 		'build_exe': {
